4cd5fd82128ef3862d3ce332b082330689a4e743
ParthTiwa-ri <parthtiwari421@gmail.com>
2024-02-03 19:05:28 +0530

    map-click

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,5 +1,28 @@
+import { RotatingLines } from "react-loader-spinner";
+
 function Loader() {
-  return <h1>Loading....</h1>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <RotatingLines
+        visible={true}
+        height="130"
+        width="130"
+        color="grey"
+        strokeWidth="5"
+        animationDuration="0.75"
+        ariaLabel="rotating-lines-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+    </div>
+  );
 }
 
 export default Loader;
